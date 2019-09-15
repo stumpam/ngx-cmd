9fc9b23819cc51feb397ea9def40077fa9548e71
Martin Štumpa <martin@stumpa.eu>
2019-09-15 22:41:57 +0200

    Fix tests with lodash

--- libs/ngx-cmd/jest.config.js
+++ libs/ngx-cmd/jest.config.js
@@ -1,9 +1,14 @@
 module.exports = {
   name: 'ngx-cmd',
-  preset: '../../jest.config.js',
+  preset: 'ts-jest',
   coverageDirectory: '../../coverage/libs/ngx-cmd',
   snapshotSerializers: [
     'jest-preset-angular/AngularSnapshotSerializer.js',
     'jest-preset-angular/HTMLCommentSerializer.js',
   ],
+  moduleNameMapper: {
+    // we'll use commonjs version of lodash for tests 👌
+    // because we don't need to use any kind of tree shaking right?!
+    '^lodash-es$': '<rootDir>/../../node_modules/lodash/index.js'
+  },
 };
