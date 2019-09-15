6801045ef4f87412c2d10820efb0d9aab81dc7e3
Martin Štumpa <martin@stumpa.eu>
2019-09-15 22:41:07 +0200

    Add on event method

--- libs/ngx-cmd/src/lib/events.service.spec.ts
+++ libs/ngx-cmd/src/lib/events.service.spec.ts
@@ -9,4 +9,29 @@ describe('EventsService', () => {
     const service: EventsService = TestBed.get(EventsService);
     expect(service).toBeTruthy();
   });
+
+  it('should return same instance', () => {
+    const service: EventsService = TestBed.get(EventsService);
+
+    const x = service.onEvt('abc');
+    const y = service.onEvt('abc');
+
+    expect(x).toEqual(y);
+
+  });
+
+  it('should clear sub', () => {
+    const service: EventsService = TestBed.get(EventsService);
+
+    expect(service['subs']['abc']).toBeUndefined();
+
+    const x = service.onEvt('abc').subscribe();
+    const y = service.onEvt('abc').subscribe();
+
+    x.unsubscribe();
+    y.unsubscribe();
+
+    expect(service['subs']).toEqual({});
+
+  });
 });
