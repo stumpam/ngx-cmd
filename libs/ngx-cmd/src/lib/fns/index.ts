9a2c27f9348576ec2fd40e547244367d9b7a516d
Martin Štumpa <martin@stumpa.eu>
2019-11-22 22:45:31 +0100

    feat: add get fn

--- libs/ngx-cmd/src/lib/fns/index.ts
+++ libs/ngx-cmd/src/lib/fns/index.ts
@@ -1,2 +1,12 @@
 export const switchcase = (cases: any) => (defaultCase: any) => (key: any) =>
   cases.hasOwnProperty(key) ? cases[key] : defaultCase;
+
+export const get = <T>(key: string, obj: T) => {
+  const currentKey = key.split('.');
+
+  const result = obj && obj[key];
+
+  return currentKey.length > 1
+    ? get(currentKey.slice(1).join('.'), result)
+    : result;
+};
